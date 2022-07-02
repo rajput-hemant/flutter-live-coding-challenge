fbc9dae91f23ed34a57e6533d1cd7a7963275a7c
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-02 20:20:39 +0530

    locked app Orientation to portrait only
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/completion_screen.dart';
 import 'screens/get_started.dart';
 import 'screens/login_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
+    return MaterialApp(
       title: 'Flutter Live Coding Challenge',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
