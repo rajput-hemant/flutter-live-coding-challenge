e89b40355975473dcbe0bbf2e8b5c90833ea212d
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-02 18:17:29 +0530

    updated get_started screen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+
 class GetStarted extends StatelessWidget {
   static const routename = '/get_started';
   const GetStarted({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Live Coding Challenge',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/bg.png'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, LoginScreen.routename),
+              style: ElevatedButton.styleFrom(
+                elevation: 10,
+                minimumSize: const Size(200, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
